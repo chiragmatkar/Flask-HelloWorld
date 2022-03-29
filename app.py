3e62f69a39414c4bf4419d66b3c8e796a71a94e7
Chirag Matkar <chirag.matkar@gmail.com>
2022-03-29 14:42:07 +0530

    testing flask app 5

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return 'Web App with Python Flask!'
 
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
